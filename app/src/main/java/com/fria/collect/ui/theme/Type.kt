bf262c12fb751cf573555eae314304681e78c50e
JeeSeongDeok <bh939@naver.com>
2023-04-18 20:30:40 +0900

    [#13] Gmarket Font 추가

--- app/src/main/java/com/fria/collect/ui/theme/Type.kt
+++ app/src/main/java/com/fria/collect/ui/theme/Type.kt
@@ -2,33 +2,26 @@ package com.fria.collect.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.fria.collect.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val Typography: Typography
+    get() = Typography(
+        bodyLarge = TextStyle(
+            fontFamily = GmarketFont,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.5.sp
+        )
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+val GmarketFont = FontFamily(
+    Font(R.font.gmarket_sans_medium, FontWeight.Normal),
+    Font(R.font.gmarket_sans_light,FontWeight.Light),
+    Font(R.font.gmarket_sans_bold, FontWeight.Bold),
 )
