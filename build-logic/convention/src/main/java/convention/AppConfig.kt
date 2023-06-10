48c330812c0b6cbe017257b93137c4de243ba065
JeeSeongDeok <bh939@naver.com>
2023-06-10 13:01:29 +0900

    [chore] targetSDK, compileSDK 34

--- build-logic/convention/src/main/java/convention/AppConfig.kt
+++ build-logic/convention/src/main/java/convention/AppConfig.kt
@@ -3,10 +3,10 @@
 import org.gradle.api.JavaVersion
 
 object AppConfig {
-    const val compileSdk = 33
+    const val compileSdk = 34
     const val buildToolsVersion = "30.0.3"
     const val minSdk = 28
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val versionCode = 1
     const val versionName = "0.0.1"
     val JAVA_VERSION = JavaVersion.VERSION_17
